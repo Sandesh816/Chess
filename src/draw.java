f371a6e9dc9e3d084a838d9c6dd3f734831a0199
Shinsaku Kataoka <shinsakukataoka@host-MacBook-Air-82-mac-d0880c779a26.eduroam.ssid.10net.amherst.edu>
2023-02-12 14:53:45 -0500

    board created

--- src/draw.java
+++ src/draw.java
@@ -1,14 +1,26 @@
 import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.Color;
 
 public class draw extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         int width = 640;
         int height = 640;
-        int x = (getWidth() - width) / 2;
-        int y = (getHeight() - height) / 2;
-        g.drawRect(x, y, width, height);
+        int squareSize = width/8;
+        g.drawRect(80, 80, width, height);
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                int s = 80 + col * squareSize;
+                int t = 80 + row * squareSize;
+                if ((row + col) % 2 == 0) {
+                    g.setColor(Color.WHITE);
+                } else {
+                    g.setColor(Color.ORANGE);
+                }
+                g.fillRect(s, t, squareSize, squareSize);
+            }
+        }
     }
 }
