6ab44e25e0d1a0650612393a9e2c1afcf8a37e04
Sandesh Ghimire <104772849+Sandesh816@users.noreply.github.com>
2023-04-18 12:15:02 -0400

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,6 +1,26 @@
-public interface Square {
-    public boolean isOccupied();
-    public Piece getPiece();
-    public void addPiece(Piece P);
-    public void removePiece();
+public class Square {
+    // Kind of like a node, each square holds a piece
+    Piece piece;
+
+    public Square(){
+        // Default piece is null
+        this.piece = null;
+    }
+    public boolean isOccupied(){
+        return piece!=null;
+    }
+    public Piece getPiece(){
+        return piece;
+    }
+    public void addPiece(Piece P){
+        if (isOccupied()){
+            removePiece();
+        }
+        this.piece= P;
+    }
+    public void removePiece(){
+        System.out.println("The "+ piece+ " has been eliminated.");
+        this.piece = null;
+    }
 }
+
