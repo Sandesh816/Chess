f12a1cf29c3d90c68e55208993b63616354a3355
Sandesh Ghimire <104772849+Sandesh816@users.noreply.github.com>
2023-09-29 17:00:31 -0400

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Board A = new Board();
-        System.out.println(A);
-        System.out.println(A.board[4][7].getPiece());
-        A.board[4][7].getPiece().move(new Pair(3,7));
-        System.out.println(A.board[3][7].getPiece());
-    }
-}
